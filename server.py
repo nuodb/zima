5ffbd9dd4b523e5d607cd898fc68f212e86757e6
Alex Charis <acharis@nuodb.com>
2015-07-17 16:54:19 -0400

    support for microbenchmarks via OAR

--- server.py
+++ server.py
@@ -14,8 +14,36 @@ SUITE_FILE = "suites.json"
 def submit_single(job_desc, branch, build_url):
     #consider NamedTuple for job_desc
     properties = "{ssd=1}/host=%s+{ssd=0}/host=%s" % (job_desc['SM'], job_desc['TE'])
-    cmd = ["oarsub", "-l", properties, "-d", "/home/build", "/home/acharis/bin/run-evil.sh {}".format(build_url)]
+    cmd = ["oarsub"]
+    cmd.append("-l")
+    cmd.append(properties)
+    cmd.append("-d")
+    cmd.append("/home/build")
+    cmd.append("/home/acharis/bin/run-evil.sh {}".format(build_url))
     #app.logger.info("command: {}".format(" ".join(cmd)))
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    (out, err) = p.communicate()
+    return {'out':out, 'err':err}
+
+
+#in bamboo:
+#curl "base/zima/enqueue_micro?branch=${bamboo.repository.branch.name}&buildid=${bamboo.buildResultKey}"
+#the buildResultKey for the benchmark run (which hasn't been created yet) is what's used for making
+#the link in core-view back to the benchmark run in bamboo...to get to the log+junit results
+#so for now, make that optional, and solve that problem a different way later
+def submit_micro(branch, build_id):
+    cmd = ["oarsub"]
+    cmd.append("-l")
+    cmd.append("/host=1")
+    cmd.append("-d")
+    cmd.append("/home/build")
+    cmd.append("/home/build/arewefastyet/kickoff-micro")
+    cmd.append("-i")
+    cmd.append("5")
+    cmd.append(build_id)
+    cmd.append("micro")
+    cmd.append(branch)
+    cmd.append("MASTER")#can't make it optional in kickoff, so hardcode since we don't gather-cores for micro anyway
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     (out, err) = p.communicate()
     return {'out':out, 'err':err}
@@ -42,6 +70,46 @@ def enqueue():
     #run oarsub lots of times...
     result = submit(suite, branch, build_url)
     return jsonify(resp=result)
+
+@app.route('/enqueue_micro')
+def enqueue_micro():
+    branch = request.args.get('branch', 'master', type=str)
+    build_id = request.args.get('buildid', type=str)
+    result = submit_micro(branch, build_id)
+    return jsonify(resp=result)
+
+# @app.route('/enqueue_single', methods=['POST'])
+# def enqueue_single():
+#     file = request.files['file']
+#     buildID = request.form.get("branch", None)
+#     email = request.form.get("email", None)
+
+@app.route('/job_status')
+def job_status():
+    return jsonify("ALEX: not yet implemented")
+
+@app.route('/queue_status')
+def queue_status():
+    return jsonify("ALEX: not yet implemented")
+
+@app.route('/node_status')
+def node_status():
+    return jsonify("ALEX: not yet implemented")
+
+@app.route('/build_status')
+def build_status():
+    #synthetic based on 'project' in the OAR job
+    return jsonify("ALEX: not yet implemented")
+
+@app.route('/junit_collect', methods=['POST'])
+def junit_collect():
+    #recv a junit file for a particular test
+    return jsonify("ALEX: not yet implemented")
+
+@app.route('/junit_serve')
+def junit_serve():
+    #prompted by bamboo, yield the junit file(s) requested
+    return jsonify("ALEX: not yet implemented")
 
 @app.route('/')
 def show_index():
