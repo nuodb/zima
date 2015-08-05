d543e6fd568623c07f581bce084a777be62675b5
Alex Charis <acharis@nuodb.com>
2015-08-05 16:09:04 -0400

    progress on server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import os, md5, re, time, json, urllib2, subprocess, logging
+import os, md5, re, time, json, urllib2, subprocess, logging, random, string
 from flask import Flask, render_template, send_from_directory, request, jsonify, g
 from artifact_link_finder import get_link, NoSuchBuildException
 from logging.handlers import RotatingFileHandler
@@ -9,66 +9,63 @@ handler = RotatingFileHandler('/tmp/zima.log', maxBytes=10000, backupCount=1)
 handler.setLevel(logging.INFO)
 app.logger.addHandler(handler)
 app.config['PROPAGATE_EXCEPTIONS'] = True
-SUITE_FILE = "suites.json"
+TEST_DIR = "/usr/local/zima/properties"
 
-def submit_single(job_desc, branch, build_url):
-    #consider NamedTuple for job_desc
-    properties = "{ssd=1}/host=%s+{ssd=0}/host=%s" % (job_desc['SM'], job_desc['TE'])
+def submit_single(test_def_fn, test_def, branch, build_result_key, token):
+    job_desc = parse_job_desc(test_def)
+    error = check_job_desc(job_desc)
+    if error:
+        return {'out':'', 'err': error}
+    properties = "{ssd=1}/host=%s+{ssd=0}/host=%s" % (job_desc['NUM_SM_HOSTS'], job_desc['NUM_TE_HOSTS'])
     cmd = ["oarsub"]
     cmd.append("-l")
     cmd.append(properties)
     cmd.append("-d")
-    cmd.append("/home/build")
-    cmd.append("/home/acharis/bin/run-evil.sh {}".format(build_url))
+    cmd.append("/var/local")
+    cmd.append("/home/build/perf/runner {} {} {}".format(test_def_fn, branch, build_result_key))
     #app.logger.info("command: {}".format(" ".join(cmd)))
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     (out, err) = p.communicate()
     return {'out':out, 'err':err}
 
-
 #in bamboo:
 #curl "base/zima/enqueue_micro?branch=${bamboo.repository.branch.name}&buildid=${bamboo.buildResultKey}"
 #the buildResultKey for the benchmark run (which hasn't been created yet) is what's used for making
-#the link in core-view back to the benchmark run in bamboo...to get to the log+junit results
+#bamboo-link in core-view back to the benchmark run in bamboo...to get to the log+junit results
 #so for now, make that optional, and solve that problem a different way later
 def submit_micro(branch, build_id):
     cmd = ["oarsub"]
     cmd.append("-l")
-    cmd.append("/host=1")
+    cmd.append("/host=1,walltime=4:0:0")
     cmd.append("-d")
     cmd.append("/home/build")
-    cmd.append("/home/build/arewefastyet/kickoff-micro")
-    cmd.append("-i")
-    cmd.append("5")
-    cmd.append(build_id)
-    cmd.append("micro")
-    cmd.append(branch)
-    cmd.append("MASTER")#can't make it optional in kickoff, so hardcode since we don't gather-cores for micro anyway
+    cmd.append("TERM=dumb /home/build/arewefastyet/kickoff-micro -i 5 {} micro {} MASTER".format(build_id, branch))
+    #can't make it optional in kickoff, so hardcode since we don't gather-cores for micro anyway
+    app.logger.info("command: {}".format(" ".join(cmd)))
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     (out, err) = p.communicate()
     return {'out':out, 'err':err}
 
-def submit(suite, branch, build_url):
-    with open(SUITE_FILE, 'r') as fd:
-        config_data = json.load(fd)
+def submit(suite, branch, build_result_key, token):
     result = {}
-    for job_desc in config_data[suite]:
-        app.logger.info("calling submit_single with job_desc: {}".format(job_desc))
-        result[job_desc['name']] = submit_single(job_desc, branch, build_url)
+    tests = [fn for fn in os.listdir(TEST_DIR) if suite in fn]
+    for fn in tests:
+        with open(fn, 'r') as fd:
+            test_def = fd.read()
+        app.logger.info("calling submit_single with job: {}".format(fn))
+        result[fn] = submit_single(fn, test_def, branch, build_result_key, token)
     return result
 
 @app.route('/enqueue')
 def enqueue():
-    app.logger.info("got request")
-    suite = request.args.get('suite', 'all', type=str)
+    suite = request.args.get('suite', '', type=str)
     branch = request.args.get('branch', 'master', type=str)
     build_result_key = request.args.get('buildResultKey', type=str)
-    try: 
+    try: #fail fast
         build_url = get_link(build_result_key)
     except NoSuchBuildException:
         return jsonify(error="no such build")
-    #run oarsub lots of times...
-    result = submit(suite, branch, build_url)
+    result = submit(suite, branch, build_result_key, get_token())
     return jsonify(resp=result)
 
 @app.route('/enqueue_micro')
@@ -84,12 +81,12 @@ def enqueue_micro():
 #     buildID = request.form.get("branch", None)
 #     email = request.form.get("email", None)
 
+@app.route('/get_test_def/<filename>')
+def get_test_def(filename):
+    return send_from_directory(TEST_DIR, filename)
+
 @app.route('/job_status')
 def job_status():
-    return jsonify("ALEX: not yet implemented")
-
-@app.route('/queue_status')
-def queue_status():
     return jsonify("ALEX: not yet implemented")
 
 @app.route('/node_status')
@@ -114,6 +111,26 @@ def junit_serve():
 @app.route('/')
 def show_index():
     return render_template('index.html')
+
+def get_token():
+    candidate = "".join(random.SystemRandom().choice(string.ascii_uppercase) for x in range(12))
+    #ALEX: if candidate safe
+    return token
+
+def parse_job_desc(test_def):
+    def parse_kv(k, _, v):
+        return k, int(v)
+    return dict([parse_kv(*x.partition("=")) for x in test_def.strip().split('\n')])
+
+def check_job_desc(job_desc):
+    if 'DISABLED' in job_desc:
+        return 'Test disabled'
+    required = ['SCRIPT', 'NUM_TE_HOSTS', 'NUM_SM_HOSTS']
+    errstr = "You must specify all of: {}".format(", ".join(required))
+    if all(key in job_desc for key in required):
+        return None
+    else:
+        return errstr
 
 if __name__=="__main__":
     handler = RotatingFileHandler('/tmp/zima.log', maxBytes=10000, backupCount=1)
