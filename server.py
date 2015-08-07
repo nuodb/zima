bf28de5f59948b8912c5569e4a54b554bc8993d2
Alex Charis <acharis@nuodb.com>
2015-08-07 16:16:36 -0400

    some progress on multihost

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import os, md5, re, time, json, urllib2, subprocess, logging, random, string
 from flask import Flask, render_template, send_from_directory, request, jsonify, g
 from artifact_link_finder import get_link, NoSuchBuildException
 from logging.handlers import RotatingFileHandler
+from threading import Lock
 
 app = Flask(__name__)
 app.debug = True
@@ -9,9 +10,11 @@ handler = RotatingFileHandler('/tmp/zima.log', maxBytes=10000, backupCount=1)
 handler.setLevel(logging.INFO)
 app.logger.addHandler(handler)
 app.config['PROPAGATE_EXCEPTIONS'] = True
-TEST_DIR = "/usr/local/zima/properties"
+#TEST_DIR = "/usr/local/zima/properties"
+TEST_DIR = "/usr/local/zima/test-properties"
+RESULT_DIR = "/usr/local/zima/results"
 
-def submit_single(test_def_fn, test_def, branch, build_result_key, token):
+def submit_single(test_def_fn, test_def, parent_build_id, token):
     job_desc = parse_job_desc(test_def)
     error = check_job_desc(job_desc)
     if error:
@@ -20,9 +23,14 @@ def submit_single(test_def_fn, test_def, branch, build_result_key, token):
     cmd = ["oarsub"]
     cmd.append("-l")
     cmd.append(properties)
+    #ALEX: set project=token
     cmd.append("-d")
     cmd.append("/var/local")
-    cmd.append("/home/build/perf/runner {} {} {}".format(test_def_fn, branch, build_result_key))
+    cmd.append("-O")
+    cmd.append("/var/local/testtmp-"+token+"/output")
+    cmd.append("-E")
+    cmd.append("/var/local/testtmp-"+token+"/error")
+    cmd.append("/home/build/perf/runner {} {} {}".format(test_def_fn, parent_build_id, token))
     #app.logger.info("command: {}".format(" ".join(cmd)))
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     (out, err) = p.communicate()
@@ -46,26 +54,28 @@ def submit_micro(branch, build_id):
     (out, err) = p.communicate()
     return {'out':out, 'err':err}
 
-def submit(suite, branch, build_result_key, token):
+def submit(suite, branch, parent_build_id, token):
+    with open(os.path.join(RESULT_DIR,token,"branch"), 'w') as fd:
+        fd.write(branch)
     result = {}
     tests = [fn for fn in os.listdir(TEST_DIR) if suite in fn]
     for fn in tests:
         with open(fn, 'r') as fd:
             test_def = fd.read()
         app.logger.info("calling submit_single with job: {}".format(fn))
-        result[fn] = submit_single(fn, test_def, branch, build_result_key, token)
+        result[fn] = submit_single(fn, test_def, parent_build_id, token)
     return result
 
 @app.route('/enqueue')
 def enqueue():
     suite = request.args.get('suite', '', type=str)
     branch = request.args.get('branch', 'master', type=str)
-    build_result_key = request.args.get('buildResultKey', type=str)
+    parent_build_id = request.args.get('buildResultKey', type=str)#bamboo id for NPB, etc
     try: #fail fast
         build_url = get_link(build_result_key)
     except NoSuchBuildException:
         return jsonify(error="no such build")
-    result = submit(suite, branch, build_result_key, get_token())
+    result = submit(suite, branch, parent_build_id, get_result_dir())
     return jsonify(resp=result)
 
 @app.route('/enqueue_micro')
@@ -75,13 +85,13 @@ def enqueue_micro():
     result = submit_micro(branch, build_id)
     return jsonify(resp=result)
 
-# @app.route('/enqueue_single', methods=['POST'])
-# def enqueue_single():
-#     file = request.files['file']
-#     buildID = request.form.get("branch", None)
-#     email = request.form.get("email", None)
+#@app.route('/enqueue_single', methods=['POST'])
+#def enqueue_single():
+#    file = request.files['file']
+#    buildID = request.form.get("branch", None)
+#    email = request.form.get("email", None)
 
-@app.route('/get_test_def/<filename>')
+@app.route('/test_def/<filename>')
 def get_test_def(filename):
     return send_from_directory(TEST_DIR, filename)
 
@@ -98,10 +108,19 @@ def build_status():
     #synthetic based on 'project' in the OAR job
     return jsonify("ALEX: not yet implemented")
 
-@app.route('/junit_collect', methods=['POST'])
-def junit_collect():
-    #recv a junit file for a particular test
-    return jsonify("ALEX: not yet implemented")
+@app.route('/artifact_collect', methods=['POST'])
+def artifact_collect():
+    artifact = request.files['artifact']
+    token = request.form.get("token", None)
+    if not token:
+        return jsonify("ERROR: missing token")
+    with open(os.path.join(RESULT_DIR,token,artifact.filename), 'w') as fd:
+        artifact.save(fd)
+    #ALEX: if done with this project:
+    #init awfy, go through files and send results, finalize awfy
+    #create junit files
+    #init bamboo MultihostTest build
+    return jsonify("OK")
 
 @app.route('/junit_serve')
 def junit_serve():
@@ -112,10 +131,12 @@ def junit_serve():
 def show_index():
     return render_template('index.html')
 
-def get_token():
-    candidate = "".join(random.SystemRandom().choice(string.ascii_uppercase) for x in range(12))
-    #ALEX: if candidate safe
-    return token
+def get_result_dir():
+    while 1:
+        candidate = "".join(random.SystemRandom().choice(string.ascii_uppercase) for x in range(12))
+        if not os.path.isdir(os.path.join(RESULT_DIR,candidate)):
+            os.mkdir(os.path.join(RESULT_DIR,candidate))
+            return candidate
 
 def parse_job_desc(test_def):
     def parse_kv(k, _, v):
